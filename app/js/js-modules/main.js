1bc44db6997b867e581fec1243831bc426be312b
NIX\mytrofanova <ekaterinamytrofanova@nixsolutions.com>
2017-10-17 12:53:08 +0300

    add navigating pages

--- app/js/js-modules/main.js
+++ app/js/js-modules/main.js
@@ -1,8 +1,16 @@
 //= js-assets/functions
-// $(document).ready(function() {
-  
-//   $('.jsCardTitle').on('click', function() {
-//     $('.jsCardList').addClass('open');
-//   });
+var links = document.getElementsByClassName("-js-goto-innerpage");
+var i;
+for (i = 0; i < links.length; i++) {  
+  links[i].addEventListener("click", function() {    
+    document.location.href = "https://mitrofan4ik.github.io/github-page2/";
+  });
+}
 
-// });
+var linksBack = document.getElementsByClassName("-js-goto-startpage");
+
+for (var i = 0; i < links.length; i++) {  
+  links[i].addEventListener("click", function() {    
+    document.location.href = "https://mitrofan4ik.github.io/github-page/";
+  });
+}
