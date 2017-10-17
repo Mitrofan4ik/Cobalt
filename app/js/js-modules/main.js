c2cd61fd3b58e4935abdeeaf268951f603145f69
NIX\mytrofanova <ekaterinamytrofanova@nixsolutions.com>
2017-10-17 13:14:19 +0300

    fixed nav pages

--- app/js/js-modules/main.js
+++ app/js/js-modules/main.js
@@ -3,14 +3,14 @@ var links = document.getElementsByClassName("-js-goto-innerpage");
 var i;
 for (i = 0; i < links.length; i++) {  
   links[i].addEventListener("click", function() {    
-    document.location.href = "https://mitrofan4ik.github.io/github-page2/";
+    document.location.href = "https://mitrofan4ik.github.io/github-page/";
   });
 }
 
 var linksBack = document.getElementsByClassName("-js-goto-startpage");
 
-for (var i = 0; i < links.length; i++) {  
-  links[i].addEventListener("click", function() {    
-    document.location.href = "https://mitrofan4ik.github.io/github-page/";
+for (var i = 0; i < linksBack.length; i++) {  
+  linksBack[i].addEventListener("click", function() {    
+    document.location.href = "https://mitrofan4ik.github.io/github-page2/";
   });
 }
